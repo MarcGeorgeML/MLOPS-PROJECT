d22ec80823a2a702f94c65d3880094623c81d6d3
Marc George <marcgeorgenow@gmail.com>
2025-10-01 07:51:25 +0530

    project structure and env are ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Marc George",
+    author_email="marcgeorgenow@example.com",
+    packages=find_packages()
+)
